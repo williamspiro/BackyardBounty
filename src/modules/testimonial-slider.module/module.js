7881c4a8f6751d1ae06169bd4bfd8576686f8024
williamspiro <willspiro@gmail.com>
2020-02-13 21:28:15 -0500

    initial stuff

---
+++ src/modules/testimonial-slider.module/module.js
@@ -0,0 +1,10 @@
+var slider = tns({
+  container: '.slider',
+  items: 1,
+  slideBy: 1,
+  autoplay: false,
+  navPosition: 'bottom',
+  controls: false,
+  arrowKeys: true,
+  mouseDrag: true,
+});
